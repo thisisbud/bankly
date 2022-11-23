3b48023c91e6f12ae82b6503fc741f6667067204
Christopher Brady <christopher.brady@thisisbud.com>
2022-11-23 15:13:15 +0000

    chore: added react and react-dom

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('app') as HTMLElement)
+
+root.render(<div>Hello world!</div>)
