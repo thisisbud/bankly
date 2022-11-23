aac631cb9e28382d8202deadb01f69a51ae0dfab
Christopher Brady <christopher.brady@thisisbud.com>
2022-11-23 15:08:01 +0000

    chore: added webpack and babel config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  devtool: 'eval',
+  devServer: {
+    compress: true,
+    port: 3001,
+    hot: true
+  },
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'app'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' })
+  ],
+  resolve: {
+    extensions: [
+      '.js', '.ts', '.jsx', '.tsx', '.json'
+    ]
+  }
+}
+
+module.exports = config
