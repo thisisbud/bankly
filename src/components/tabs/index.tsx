280c0b4bf5ef9fd5ebe39f9037fa20e3dfac5379
Christopher Brady <christopher.brady@thisisbud.com>
2022-11-30 09:40:35 +0000

    chore: added tabs component

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
-import styled from 'styled-components'
 
 type Props = {
   children: React.ReactNode[],
   activeTab: string,
   onChange(e: React.MouseEvent<HTMLButtonElement>): void
 }
-
-const StyledTabs = styled.nav`
-  padding: 10px 0;
-`
 
 export default function Tabs(props: Props): React.ReactElement {
   const { children, onChange, activeTab } = props
@@ -18,6 +13,7 @@ export default function Tabs(props: Props): React.ReactElement {
     if (typeof child === 'undefined' || !child) {
       return
     }
+
     return React.cloneElement(child as React.ReactElement, {
       onClick: onChange,
       isActive: (child as React.ReactElement).props.value === activeTab,
@@ -25,8 +21,8 @@ export default function Tabs(props: Props): React.ReactElement {
   }
 
   return (
-    <StyledTabs>
+    <nav>
       {React.Children.map(children, renderChild)}
-    </StyledTabs>
+    </nav>
   )
 }
--- src/components/tabs/tab.tsx
+++ src/components/tabs/tab.tsx
@@ -8,23 +8,29 @@ type Props = {
   onClick?(event: React.MouseEvent<HTMLButtonElement>): void,
 }
 
-const StyledButton = styled.button<{ $isActive?: boolean }>`
-  background-color: white;
-  border-width: 0;
-  border-bottom-width: 3px;
-  border-color: transparent;
-  font-size: ${props => props.theme.fontSizeDefault};
-  padding: 0;
+const StyledRoot = styled.button<{ $isActive?: boolean }>`
+  border-width: 1px;
+  border-style: solid;
+  font-size: ${({ theme }) => theme.fontSizeDefault};
+  padding: 8px 24px;
+  line-height: 24px;
+  border-radius: 8px;
   box-sizing: border-box;
   text-decoration: none;
   cursor: pointer;
   ${({ $isActive }) => $isActive
     ? css`
-    border-bottom-color: black;
-  ` : ''}
+    background-color: ${({ theme }) => theme.colorBackgroundTabPrimaryDefault};
+    border-color: ${({ theme }) => theme.colorBorderTabPrimaryDefault};
+    color: ${({ theme }) => theme.colorTextTabPrimaryDefault};
+  ` : css`
+    background-color: ${({ theme }) => theme.colorBackgroundTabSecondaryDefault};
+    border-color: ${({ theme }) => theme.colorBorderTabSecondaryDefault};
+    color: ${({ theme }) => theme.colorTextTabSecondaryDefault};
+  `}
 
   & + & {
-    margin-left: 20px;
+    margin-left: 12px;
   }
 `
 
@@ -32,6 +38,6 @@ export default function Tab(props: Props): React.ReactElement {
   const { children, value, onClick, isActive } = props
 
   return (
-    <StyledButton $isActive={isActive} onClick={onClick} value={value}>{children}</StyledButton>
+    <StyledRoot $isActive={isActive} onClick={onClick} value={value}>{children}</StyledRoot>
   )
 }
