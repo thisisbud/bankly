deba152670eaf48dd9567c1381dbdc846f1b5840
Ollie W <ollie.williams@thisisbud.com>
2022-12-06 23:05:07 +0000

    chore: added basic global styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./app.css";
 import { Home } from "./views/home";
 
 const App = () => (
   <div className="app">
-    <a href="https://www.thisisbud.com/" target="_blank" rel="noreferrer">
-      <img src="/bud.svg" className="logo" alt="Bud logo" />
-    </a>
-    <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-    </a>
+    <div className="app__row">
+      <a href="https://www.thisisbud.com/" target="_blank" rel="noreferrer">
+        <img src="/bud.svg" className="logo" alt="Bud logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
     <Home />
   </div>
 );
