26efc27a98926f19ca0b712879b7ad38cb05e8ac
Ollie W <ollie.williams@thisisbud.com>
2022-12-07 12:17:32 +0000

    chore: removed h3 from accounts component

--- src/components/accounts/item.tsx
+++ src/components/accounts/item.tsx
@@ -9,9 +9,7 @@ export const AccountItem = ({ account }: Props) => {
   return (
     <div className="account">
       <div className="total">Total {account.balance.amount.currency}</div>
-      <h3>
-        <strong>{account.balance.amount.value}</strong>
-      </h3>
+      <strong>{account.balance.amount.value}</strong>
     </div>
   );
 };
