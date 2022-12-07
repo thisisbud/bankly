bdbdc652d14213f616e02c05a3785b3fcce34537
Ollie W <ollie.williams@thisisbud.com>
2022-12-07 12:16:36 +0000

    test: added handler tests

---
+++ src/api/handlers.test.tsx
@@ -0,0 +1,33 @@
+import { rest } from "msw";
+import { server } from "../../jest.setup";
+import { accounts } from "./data/accounts";
+import { transactions } from "./data/transactions";
+
+describe("/api/", () => {
+  test("/accounts", async () => {
+    const response = await fetch("/api/accounts");
+
+    expect(await response.json()).toEqual(accounts);
+  });
+
+  test("/transactions", async () => {
+    const response = await fetch("/api/transactions");
+
+    expect(await response.json()).toEqual(transactions);
+  });
+});
+
+describe("An example of playing around with MSW", () => {
+  test("expect /api/accounts to return foo", async () => {
+    // overwrite the existing api handler with a new one, just for this test
+    server.use(
+      rest.get("/api/accounts", (req, res, ctx) =>
+        res(ctx.status(200), ctx.json("foo"))
+      )
+    );
+
+    const response = await fetch("/api/accounts");
+
+    expect(await response.json()).toEqual("foo");
+  });
+});
--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -2,13 +2,18 @@ import { rest } from "msw";
 import { accounts } from "./data/accounts";
 import { transactions } from "./data/transactions";
 
-const duration = process.env.NODE_ENV === "test" ? 0 : 2000;
+const isTest = process.env.NODE_ENV === "test";
+const duration = isTest ? 0 : 2000;
+
+const statusCode = 200;
 
 export const handlers = [
+  // this api almost instant
   rest.get("/api/accounts", (req, res, ctx) =>
-    res(ctx.delay(duration), ctx.status(200), ctx.json(accounts))
+    res(ctx.delay(), ctx.status(statusCode), ctx.json(accounts))
   ),
+  // this api takes two seconds
   rest.get("/api/transactions", (req, res, ctx) =>
-    res(ctx.delay(duration), ctx.status(200), ctx.json(transactions))
+    res(ctx.delay(duration), ctx.status(statusCode), ctx.json(transactions))
   ),
 ];
